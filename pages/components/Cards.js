166bdf8ada26f7daa10285e30e2e600f5c420969
omkute <omkute636@gmail.com>
2022-11-12 16:55:50 +0530

    this is version one 1

---
+++ pages/components/Cards.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Image from 'next/image'
+
+
+import design from "../../public/design.png"
+
+function Cards() {
+  return (
+    <div>      
+              {/* Cards */}
+    <div className='pb-9'>
+      <div className='text-center shadow-xl rounded-xl py-4'>
+        <Image
+        src={design}
+        height="100pxss"
+        width="100px"
+
+        />
+        <h3 className='my-10 font-medium p-t8'>Beautyful designs</h3>
+        <p className="py-2 sm: px-4">Creating Eligant designs suited for your 
+        needs following core desing theory</p>
+        <h4 className='py-4 text-cyan-600 sm: px-4' >Programming Language's I use</h4>
+        <p className='py-1 text-gray-800' >Html</p>
+        <p className='py-1 text-gray-800' >CSS</p>
+        <p className='py-1 text-gray-800' >Javascript</p>
+      </div>
+    </div></div>
+  )
+}
+
+export default Cards
--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,80 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import Header from "./components/Header";
-import myimg from './assets/Snapchat-604200710.jpg'
-import Footer from "./components/Footer";
+import Image from 'next/image'
+
+import {BsMoonStarsFill} from "react-icons/bs"
+import {AiFillTwitterCircle,AiFillLinkedin,AiFillYoutube} from "react-icons/Ai"
+import vector from "../public/dev-ed-wave.png"
+import elon_img from "../public/elon.jpg"
+import design from "../public/design.png"
+import Cards from './components/Cards'
+
+
+
 
 export default function Home() {
   return (
     <div>
-      <Header/>
-    
-      <section class="text-gray-600 body-font">
-        <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-          <img
-            class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded-full"
-            alt="hero"
-            src="https://pbs.twimg.com/profile_images/1561543832474230785/78NBH2XZ_400x400.jpg"
-          />
-          <div class="text-center lg:w-2/3 w-full">
-            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              Hi, I am OM
-            </h1>
-            <p class="mb-8 leading-relaxed">
-              I am 2nd year engineering student studiyang comuter science. <br />
-              I like development, currenty I am learning web development.
-
-            </p>
-
-            <section class="text-gray-600 body-font">
- 
-    
-    
-     
-  
-</section>
+      
+      <title>Om Kute</title>
+      <main className="bg-white px-10 ">
+        <section className="  h-screen">
+          {/* navbar */}
+          <nav className="py-10 mb-12 flex justify-between">
             
-          </div>
-        </div>
-      </section>
 
-      <Footer/>
+            
+            <h1 className="text-xl font-burton">portfolio</h1>
+            <ul className="flex items-center ">
+              <li className="px-3">
+              <BsMoonStarsFill className="cursor-pointer" />
+              </li>
+              <li><a href="#" className="bg-gradient-to-r  from-cyan-500 to-teal-500 p-2 rounded text-white text-md ml-6">resume</a></li>
+            </ul>
+          </nav>
+          <div>
+            <div className=" text-center p-10 ">
+              {/* flex div for md */}
+            <div className=" md:flex items-center ">
+            {/* banner image */}
+          <Image
+            src={vector}
+            className="block"
+            />
+            <div className="icon_info pt-9">
+
+            
+
+            {/* TODo-make h2 graditent like apple */}
+            <div className="div2">
+            <h2 className="text-5xl text-cyan-500">Om Santosh Kute</h2>
+            <h3 className="text-2xl text" >A Engineering Student studying Computer Science</h3>
+            <p className="text-md py-5 text-gray-800">
+              Hey, I am Computer Science Student ,Studiying at 
+              Anuradha Engineering College.
+              Currently I am Learning web development and java along with DSA
+            </p>
+          </div>
+          {/* Socila Icons */}
+          <div className="flex text-5xl justify-center gap-x-7">
+              <AiFillTwitterCircle/>
+              <AiFillLinkedin/>
+              <AiFillYoutube/>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+
+        {/* Section 2 Cards */}
+          <section className=''>
+            <h3 className='text-center pt-6 font-bold text-xl' >Projects</h3>
+            <div className=" md:flex-5 sm:flex ">
+              <Cards className="md:px-5" />
+              <Cards className="md:px-5" />
+              <Cards className="md:px-5" />
+              </div>
+          </section>
+        </section>
+      </main>
     </div>
   );
 }
